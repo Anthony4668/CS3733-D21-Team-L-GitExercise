2f290f0870e36013d95cd1d99eb96c19a9f9c8b4
Conor <cemcdonough@wpi.edu>
2021-03-29 21:55:15 -0400

    Message Here

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -19,4 +19,7 @@ public class Main {
   public void printBranchAL() {
     System.out.println("Branch - Anthony LoPresti");
   }
+  public void printBranchCM() {
+    System.out.println("Branch - Conor McDonough");
+  }
 }
